4c53e8ebaab7fc39cc09b412bb617f8a9815e899
Ivan Sokolović <isokolovic1@gmail.com>
2021-10-21 18:16:18 +0200

    Add project files.

---
+++ KompleksniBroj/Klasa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KompleksniBroj
+{
+    class KompleksniBrojKlasa
+    {
+
+
+        public KompleksniBrojKlasa(double relalni, double imaginarni)
+        {
+            RealniDio = relalni;
+            ImaginarniDio = imaginarni;
+        }
+
+        public double RealniDio;
+        public double ImaginarniDio;
+
+
+        public override string ToString()
+        {
+            return $"({RealniDio}, {ImaginarniDio})";
+        }
+    }
+}
---
+++ KompleksniBroj/Struktura.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KompleksniBroj
+{
+    struct KompleksniBrojStrukture
+    {
+        public double RealniDio;
+        public double ImaginarniDio;
+
+        public KompleksniBrojStrukture(double realni, double imaginarni)
+        {
+            RealniDio = realni;
+            ImaginarniDio = imaginarni;
+        }
+
+        public override string ToString()
+        {
+            return $"({RealniDio}, {ImaginarniDio})";
+        }
+    }
+}
